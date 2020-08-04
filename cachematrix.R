15e5ae22a664a93d3ea4b353d14b5c97a23a1298
jhonatansgg <67992725+jhonatansgg@users.noreply.github.com>
2020-08-03 22:24:44 -0500

    My work programming Assignment 2: Lexical Scoping

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,43 @@
-## Put comments here that give an overall description of what your
-## functions do
 
-## Write a short comment describing this function
+## Estas funciones se utilizan para crear un objeto especial
+##que almacena una matriz  y la inversa en la memoria caché.
+
+## Esta funcion permite la construccion de una matriz
+#que contiene una funcion para:
+#establecer el valor de la matriz
+#obtener el valor de la matriz
+#establecer el valor de la inversa
+#obtener el valor de la inversa
 
 makeCacheMatrix <- function(x = matrix()) {
+     inv <- NULL
+     set <- function(y) {
+          x <<- y
+          inv <<- NULL
 
+     }
+     get <- function() {x}
+     setInverse <- function(inverse) {inv <<- inverse}
+     getInverse <- function() {inv}
+     list(set = set, get = get,
+          setInverse = setInverse,
+          getInverse = getInverse)
 }
 
-
-## Write a short comment describing this function
+## Esta funcion calcula la inversa de la matriz especial creada con la función anterior.
+#primero verifica si la inversa ya se ha calculado. Si es así, obtiene la inversa del caché
+#y omite el cálculo. De lo contrario, calcula la inversa de los datos y establece el valor de la
+#inversa en la memoria caché.
 
 cacheSolve <- function(x, ...) {
+     inv <- x$getInverse()
+     if(!is.null(inv)) {
+          message("getting cached data")
+          return(inv)
+     }
+     mat <- x$get()
+     inv <- solve(mat, ...)
+     x$setInverse(inv)
+     inv
         ## Return a matrix that is the inverse of 'x'
 }
